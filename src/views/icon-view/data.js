2b2b11d807e8f8fb4172e5985c20e541c379e845
LeungZhenPang <573750655@qq.com>
2019-12-11 18:20:46 +0800

    完成大部分网站数据

--- src/views/icon-view/data.js
+++ src/views/icon-view/data.js
@@ -25,30 +25,6 @@ export default {
             link: "https://www.zhihu.com"
         },
         {
-            name: "腾讯视频",
-            description: "腾讯视频致力于打造中国领先的在线视频媒体平台,以丰富的内容、极致的观看体验",
-            pic: "tengxunshipin",
-            link: "https://v.qq.com"
-        },
-        {
-            name: "爱奇艺",
-            description: "爱奇艺是拥有海量、优质、高清的网络视频的大型视频网站,专业的网络视频播放平台",
-            pic: "iqiyi",
-            link: "https://www.iqiyi.com"
-        },
-        {
-            name: "哔哩哔哩",
-            description: "bilibili是国内知名的视频弹幕网站,这里有最及时的动漫新番,最棒的ACG氛围,最有创意的Up主。",
-            pic: "bilibili",
-            link: "https://www.bilibili.com"
-        },
-        {
-            name: "优酷",
-            description: "频服务平台,提供视频播放,视频发布,视频搜索,视频分享",
-            pic: "youku",
-            link: "https://www.youku.com"
-        },
-        {
             name: "搜狐",
             description: "搜狐网为用户提供24小时不间断的最新资讯,及搜索、邮件等网络服务。内容包括全球热点事件、突发新闻",
             pic: "sohu",
@@ -103,7 +79,513 @@ export default {
             description: "果壳网是一个泛科技主题网站,提供负责任、有智趣、贴近生活的内容,你可以在这里阅读",
             pic: "guoke",
             link: "http://www.guokr.com"
+        },
+        {
+            name: "煎蛋",
+            description: "煎蛋以译介方式传播网络新鲜资讯... 是不是该抓一抓网站的运营情况了?是不是该提高一下审核人员的..",
+            pic: "jiandan",
+            link: "http://jandan.net"
+        },
+        {
+            name: "虎嗅",
+            description: "聚合优质的创新信息与人群，捕获精选|深度|犀利的商业科技资讯。在虎嗅，不错过互联网的每个重要时刻。",
+            pic: "huxiu",
+            link: "https://www.huxiu.com"
+        },
+        {
+            name: "音悦Tai",
+            description: "音悦Tai-是以高清MV为主的娱乐视频网站，提供高品质音乐视频在线观看服务。同时也是偶像和粉丝的聚集地，既可以购买明星专辑和周边，也可以参与各式各样的线下活动。",
+            pic: "yinyue",
+            link: "http://www.yinyuetai.com"
+        },
+        {
+            name: "好奇心研究",
+            description: "好奇心日报以商业视角观察生活并启发你的好奇心，囊括商业报道、科技新闻、生活方式等各个领域，致力成为这个时代最好的媒体，为用户提供最好的新闻资讯。",
+            pic: "haoqixinyanjiusuo",
+            link: "http://www.qdaily.com"
+        },
+        {
+            name: "丁香园",
+            description: "医学,药学,生物,医学论坛,医生,临床,医学知识,病例讨论,医学考试,论文,实验方法,数据库,文献,疾病,社区,用药助手,丁香园",
+            pic: "dingxiangyuan",
+            link: "http://www.dxy.cn"
+        },
+        {
+            name: "壹心理",
+            description: "壹心理，2000万人选择的心理学服务平台。你内心困惑的问题，在壹心理可以找到答案。我们提供系统的心理健康服务：包括心理学课程、心理咨询、心理测评、心理FM以及心理科普文章。",
+            pic: "yixinli",
+            link: "https://www.xinli001.com"
+        },
+        {
+            name: "多邻国",
+            description: "多邻国是世界上最受欢迎的语言学习平台。免费、有趣、科学。现在就登录 duolingo.com 或手机应用开始学习吧!",
+            pic: "duolinguo",
+            link: "http://www.duolingo.cn/"
+        },
+        {
+            name: "维基百科",
+            description: "甩百度百科几条街，可惜中文版被墙，怎么上自己想",
+            pic: "weijibaike",
+            link: "https://zh.wikipedia.org/"
+        },
+        {
+            name: "追剧",
+            description: "Neets追剧管理服务，旨在提供影视主题订阅、更新推送、网页聚合搜索。为广大动漫、美剧、日剧、韩剧、国产剧等爱好者，带来更愉快的追剧体验！",
+            pic: "zhuiju",
+            link: "https://neets.cc/"
+        },
+        {
+            name: "豆瓣FM",
+            description: "让好音乐继续 豆瓣精选",
+            pic: "doubanfm",
+            link: "https://douban.fm/"
+        },
+        {
+            name: "TypingClub",
+            description: "通过684个小关卡,提高打字速度,摆脱“二指禅”想起了曾经微机课上,被金山打字通支配的恐惧",
+            pic: "typingclub",
+            link: "https://www.typingclub.com/"
+        },
+        {
+            name: "中国大学",
+            description: "中国大学MOOC(慕课) 是国内优质的中文MOOC学习平台,由爱课程网携手网易云课堂打造。平台拥有包括985高校在内提供的千余门课程,其中首批获得认定的国家精品在线开放",
+            pic: "zhongguodaxue",
+            link: "https://www.icourse163.org/"
+        },
+        {
+            name: "我要自学网",
+            description: "我要自学网-视频教程|免费教程|自学电脑|3D教程|平面教程|影视动画教程|办公教程|机械设计教程|网站设计教程,我要自学网,视频教程",
+            pic: "woyaozixuewang",
+            link: "http://www.51zxw.net/"
+        },
+        {
+            name: "网易公开课",
+            description: "网易视频公开课频道推出国内外名校公开课,涉及广泛的学科,名校老师认真讲解深度剖析,网易视频公开课频道搭建起..",
+            pic: "wangyigongkaike",
+            link: "https://open.163.com/"
+        },
+        {
+            name: "一纳米学习",
+            description: "1纳米学习导航，最专业的学习网站导航。收集国内外最新最酷的学习网站！学习必备！找资料必备！",
+            pic: "yinamixuexi",
+            link: "http://1nami.com/"
         }
     ],
+    shipin: [
+        {
+            name: "腾讯视频",
+            description: "腾讯视频致力于打造中国领先的在线视频媒体平台,以丰富的内容、极致的观看体验",
+            pic: "tengxunshipin",
+            link: "https://v.qq.com"
+        },
+        {
+            name: "爱奇艺",
+            description: "爱奇艺是拥有海量、优质、高清的网络视频的大型视频网站,专业的网络视频播放平台",
+            pic: "iqiyi",
+            link: "https://www.iqiyi.com"
+        },
+        {
+            name: "哔哩哔哩",
+            description: "bilibili是国内知名的视频弹幕网站,这里有最及时的动漫新番,最棒的ACG氛围,最有创意的Up主。",
+            pic: "bilibili",
+            link: "https://www.bilibili.com"
+        },
+        {
+            name: "优酷",
+            description: "频服务平台,提供视频播放,视频发布,视频搜索,视频分享",
+            pic: "youku",
+            link: "https://www.youku.com"
+        }
+    ],
+    dianying: [
+        {
+            name: "音范丝",
+            description: "影音集是一个电影爱好者的资讯分享站，推荐优秀电影，人工精选高分影片，部部精品。",
+            pic: "yinfansi",
+            link: "https://www.yinfans.me/"
+        },
+        {
+            name: "电影天堂",
+            description: "最好的迅雷电影下载网，分享最新电影，高清电影、综艺、动漫、电视剧等下载",
+            pic: "dianyingtiantang",
+            link: "https://www.dytt8.net/"
+        },
+        {
+            name: "电影蜜蜂",
+            description: "为您提供高清电影资源,好看的电影推荐.在这里您可以找到世界各国好看的电影,以及各类..",
+            pic: "dianyingmifeng",
+            link: "https://www.dybee.tv/"
+        },
+        {
+            name: "泡饭影视",
+            description: "收罗全网高清资源下载链接的资源博客，院线大片，美剧，英剧，日剧，韩剧，港台剧，大陆剧，应有尽有！",
+            pic: "paofanyingshi",
+            link: "http://www.chapaofan.com/"
+        },
+        {
+            name: "电影FM",
+            description: "一个专注于电影推荐的网站，通过记录和分析你看过的电影，找出符合你口味的推荐，你还可以制作属于自己的海报墙，拥有自己的电影主页。电影FM还提供电影播放地址及磁力链接下载。",
+            pic: "dianyingfm",
+            link: "http://www.dianying.fm/"
+        },
+        {
+            name: "飘花电影",
+            description: "最新电影,3E迅雷电影下载网(原3edyy.com)-每天搜集最新迅雷免费电影下载。为使用迅雷软件的用户提供最新的免费电影下载、小电影下载、高清电影下载等服务。",
+            pic: "piaohuadianying",
+            link: "https://www.piaohua.com/"
+        },
+        {
+            name: "BT电影天堂",
+            description: "为广大影迷朋友们提供最新最热门的迅雷电影下载。我们专注于高清电影下载服务，全力打造优秀的BT种子、迅雷种子下载网站！",
+            pic: "btdianyingtiantang",
+            link: "http://www.btbtdy.me/"
+        },
+        {
+            name: "葡萄影视",
+            description: "国内更新速度最快最全的，2017最新电影，韩国电视剧、香港TVB电视剧、韩剧、日剧、美剧、综艺的在线观看和剧集交流场所，在线观看有吉吉影音、西瓜影音、迅雷下载等播放高清播放模式，葡萄影视网",
+            pic: "putaoyingshi",
+            link: "http://www.putaoys.com/"
+        }
+    ],
+    yinyue: [
+        {
+            name: "网易云音乐",
+            description: "一款专注于发现与分享的音乐产品,依托专业音乐人、DJ、好友推荐及社交功能,为用户打造全新的音乐生活。",
+            pic: "wangyiyunyinyue",
+            link: "https://music.163.com/"
+        },
+        {
+            name: "酷狗音月",
+            description: "为您免费提供最全的在线音乐试听下载,以及全球海量电台和MV播放服务、最新音乐播放器下载。酷狗音乐 和音乐在一起。",
+            pic: "kugouyinyue",
+            link: "http://www.kugou.com/"
+        },
+        {
+            name: "QQ音乐",
+            description: "海量音乐在线试听、新歌热歌在线首发、歌词翻译、手机铃声下载、高品质无损音乐试听、海量无损曲库、正版音乐下载、空间...",
+            pic: "qqyinyue",
+            link: "https://y.qq.com/"
+        },
+        {
+            name: "虾米音乐",
+            description: "提供高品质音乐MP3的个性化推荐、发布、P2P下载服务,以及线下音乐活动等互动内容... 基于你最近听的音乐 今日推荐歌单 收藏 添加到 分享到基于最近流行为你推荐不感兴...",
+            pic: "xiamiyinyue",
+            link: "https://www.xiami.com/"
+        },
+        {
+            name: "9酷音乐",
+            description: "专业的在线音乐试听mp3下载网站.收录了网上最新歌曲和流行音乐,网络歌曲,好听的歌,非主流音乐,QQ音乐,经典老歌,劲舞团歌曲,搞笑歌曲,儿童..",
+            pic: "9kuyinyue",
+            link: "http://www.9ku.com/"
+        },
+        {
+            name: "咪咕音乐",
+            description: "为您打造音乐、视频、 阅读、游戏、动漫的数字内容聚合平台 给您全新的娱乐体验。",
+            pic: "miguyinyue",
+            link: "http://www.migu.cn//"
+        },
+        {
+            name: "千千音乐",
+            description: "中国音乐门户之一,为你提供海量正版高品质音乐,权威的音乐榜单,快速的新歌速递,契合你的主题电台,人性化的歌曲搜索,让你更快地找到喜爱的音乐",
+            pic: "qianqianyinyue",
+            link: "http://music.taihe.com/"
+        },
+        {
+            name: "echo",
+            description: "年轻人里最流行的音乐APP之一,也是全球最大的3D音乐平台。echo将颠覆你对音乐的一切体验。在这里,发现前所未有的好听音乐、3D神奇立体音效、弹幕评论..",
+            pic: "echo",
+            link: "http://www.app-echo.com"
+        }
+    ],
+    zhibo: [
+        {
+            name: "斗鱼",
+            description: "每个人的直播平台提供高清、快捷、流畅的视频直播和游戏赛事直播服务,包含英雄联盟lol直播、穿越火线cf直播、dota2直播、美女直播等各类热门游戏赛事直播和各种.",
+            pic: "douyuzhibo",
+            link: "https://www.douyu.com/"
+        },
+        {
+            name: "虎牙",
+            description: "以游戏直播为主的弹幕式互动直播平台,累计注册用户2亿,提供热门游戏直播、电竞赛事直播与游戏赛事直播,手游直播等。包含王者荣耀,绝地求生,荒野行动等游戏...",
+            pic: "huyazhibo",
+            link: "http://www.huya.com/"
+        },
+        {
+            name: "YY",
+            description: "致力于打造全民娱乐的互动直播平台,以多样的美女互动、优质的直播内容、极致的互动体验,满足用户音乐、舞蹈、户外等直播及绝地求生、王者荣耀等热门游戏直播的观看...",
+            pic: "yyzhibo",
+            link: "http://www.yy.com/"
+        },
+        {
+            name: "战旗直播",
+            description: "提供高清、流畅的视频直播和电子竞技游戏直播。包括三国杀直播、LOL英雄联盟直播、炉石传说直播、dota2直播等各类热门游戏赛事。战旗直播为您提供全新的...",
+            pic: "zhanqizhibo",
+            link: "https://www.zhanqi.tv/"
+        },
+        {
+            name: "花椒直播",
+            description: "一个聚集超高颜值明星大咖、美女帅哥、热门网红、校花校草、逗比萌妹的手机直播社交平台,这里有明星发布会、花边新闻、才艺展示、生活趣闻、聊天互、唱歌...",
+            pic: "huajiaozhibo",
+            link: "https://www.huajiao.com/"
+        },
+        {
+            name: "章鱼直播",
+            description: "全民原创互动的体育直播,为观众提供覆盖面最广的比赛和多样化的直播体验。章鱼的直播内容包括中超、英超...",
+            pic: "zhangyuzhibo",
+            link: "http://www.zhangyu.tv/"
+        },
+        {
+            name: "九秀网",
+            description: "集视频交友与美女在线表演为一体的视频交友网站,这里有美女视频直播、美女视频聊天、美女视频表演,看美女主播,与美女视频互动。九秀,一个美女如云网站!",
+            pic: "jiuxiuwang",
+            link: "http://www.9xiu.com/"
+        }
+    ],
+    nansheng: [
+        {
+            name: "有货",
+            description: "年轻人潮流购物中心，中国潮流商品购物网站。作为YOHO!旗下的购物平台，汇集了全球潮流时尚商品和国内流行商品，★全场品牌正品保证，支持货到付款★",
+            pic: "youhuo",
+            link: "https://www.yohobuy.com/"
+        },
+        {
+            name: "虎扑",
+            description: "一个认真而有趣的社区,每天有众多JRs在虎扑分享自己对篮球、足球、游戏电竞、运动装备、影视、汽车、数码、情感等一切人和事的见解,热闹、真实、有温度。",
+            pic: "hupu",
+            link: "https://www.hupu.com/"
+        },
+        {
+            name: "直播吧",
+            description: "知名体育平台,主要为足球迷,篮球迷等提供直播,新闻资讯,数据,论坛社区,原创报道,视频集锦等服务",
+            pic: "zhiboba",
+            link: "https://www.zhibo8.cc/"
+        },
+        {
+            name: "太平洋电脑",
+            description: "专业IT门户网站,为用户和经销商提供IT资讯和行情报价,涉及电脑,手机,数码产品,软件等.",
+            pic: "taipingyangdiannao",
+            link: "https://www.pconline.com.cn/"
+        },
+        {
+            name: "多玩游戏",
+            description: "国内专业的游戏门户，玩家论坛与视频网站。提供了专业的游戏资讯、强大的游戏攻略、游戏工具盒子和大量的ACG视频。多玩游戏，多交朋友，多看视频。",
+            pic: "duowanyouxi",
+            link: "http://www.duowan.com/"
+        },
+        {
+            name: "龍柒",
+            description: "CONVERSE 携手潮流球鞋店 SNEAKERSNSTUFF 全新 诠释 ONE STAR 鞋款 群雄逐鹿,Air Jordan NBA Pack 灵感爆发,Air Jordan 3 “Chlorophyll” 夏日冲击,adidas Yu..",
+            pic: "longqi",
+            link: "http://www.long7.com/"
+        },
+        {
+            name: "TapTap",
+            description: "一个推荐高品质手游的手游分享社区，实时同步全球各大应用市场游戏排行榜，与全球玩家共同交流并发掘高品质手游。每一款推荐游戏，都是由专业的测评团队从全球海量的游戏中精选而出，只为你提供好玩的手机游戏。",
+            pic: "taptap",
+            link: "https://www.taptap.com/"
+        },
+        {
+            name: "新新球鞋网",
+            description: "国内最具有影响力的著名运动鞋文化网站之一，主体定位于运动咨询类服务，目的就是让更多的朋友了解认识运动鞋及相关方面的知识，提供最新、最及时的咨询信息，让大家在运动的同时了解运动产品及其相关的方方面面信息。",
+            pic: "xinxinqiuxiewang",
+            link: "https://www.x-kicks.com/"
+        },
+        {
+            name: "Chiphell",
+            description: "比较豪的评测社区，什么都有",
+            pic: "chiphell",
+            link: "https://www.chiphell.com/"
+        },
+        {
+            name: "汽车之家",
+            description: "为您提供最新汽车报价，汽车图片，汽车价格大全，最精彩的汽车新闻、行情、评测、导购内容，是提供信息最快最全的中国汽车网站。",
+            pic: "qichezhijia",
+            link: "https://www.autohome.com.cn/"
+        },
+        {
+            name: "天涯社区",
+            description: "网络人物网络事件的发源地, 网络名词网络流行语的创造者.",
+            pic: "tianyashequ",
+            link: "http://focus.tianya.cn/"
+        },
+        {
+            name: "同花顺财经",
+            description: "一家专业的互联网金融数据服务商,为您全方位提供财经资讯及全球金融市场行情,覆盖股票、基金、期货、外汇、...",
+            pic: "tonghuashuncaijing",
+            link: "http://www.10jqka.com.cn/"
+        },
+        {
+            name: "中关村在线",
+            description: "中国领先的IT信息与商务门户, 包括新闻, 商城, 硬件, 下载, 游戏, 手机, 评测等40个大型频道，每天发布大量各类产品促销信息及文章专题，是IT行业的厂商, 经销商, IT产品, 解决方案的提供场所",
+            pic: "zhongguancunzaixian",
+            link: "http://www.zol.com.cn/"
+        },
+        {
+            name: "机锋网",
+            description: "中国最大的Android安卓平台,中关村智造大街唯一入驻媒体。也是国内唯一 一家定位在手机机友玩家、手机发烧友的服务平台。作为安卓手机玩家基地,机锋坚持为中国对...",
+            pic: "jifengwang",
+            link: "http://www.gfan.com/"
+        },
+        {
+            name: "雷锋网",
+            description: "国内最早关注人工智能和智能硬件领域的互联网科技媒体，内容涵盖人工智能、智能硬件、机器人、智能驾驶、ARVR、网络安全、物联网、未来医疗、金融科技等9大领域。雷锋网致力于连接和服务学术界、工业界与投资界，为用户提供更专",
+            pic: "leifengwang",
+            link: "https://www.leiphone.com/"
+        },
+        {
+            name: "ZEALER",
+            description: "中国具有影响力的科技视频平台。创始人兼CEO王自如 和 ZEALER 坚持打造兼具行业洞察力与品质感的科技视频内容,输出「科技 Plus」生活主张。目前涵盖消费电子、智能..",
+            pic: "zealer",
+            link: "http://www.zealer.com/"
+        }
+    ],
+    nvsheng: [
+        {
+            name: "蘑菇街",
+            description: "中国领先的时尚目的地。公司通过形式多样的时尚内容，种类丰富的时尚商品，以及结合红人直播、买手选款+智能推荐的售卖方式，让人们在分享和发现流行趋势的同时，尽情享受优质的购物体验。2011年，蘑菇街正式上线",
+            pic: "mogujie",
+            link: "https://www.mogujie.com/"
+        },
+        {
+            name: "太平洋时尚",
+            description: "集时髦、实用、时效为一体的全明星时尚网络和专业女性资讯平台。着力打造时尚、独立、健康的新时代女性，传递女性期待的一切。PClady提供专业的美容消费指南、解读时髦的时装趋势",
+            pic: "taipingyangshishang",
+            link: "https://www.pclady.com.cn/"
+        },
+        {
+            name: "唯品会",
+            description: "以1-7折超低折扣对全球各大品牌进行限时特卖，商品囊括服装、化妆品、家居、奢侈品等上千品牌。100%正品、低价、货到付款、7天无理由退货。",
+            pic: "weipinhui",
+            link: "https://www.vip.com/"
+        },
+        {
+            name: "下厨房",
+            description: "美食菜谱网倡导在家烹饪、健康的生活方式,提供有版权的实用菜谱做法与饮食知识,提供厨师和美食爱好者一个记录、分享的平台。",
+            pic: "xiachufang",
+            link: "http://www.xiachufang.com/"
+        },
+        {
+            name: "Onlylady",
+            description: "女人志女性时尚生活平台是专业的女性时尚网站，为广大用户提供专业的时尚潮流、美容方法、流行趋势、服饰时装资讯，打造专业时尚、美容、生活、达人、互动平台。",
+            pic: "onlylady",
+            link: "http://www.onlylady.com/"
+        },
+        {
+            name: "红袖添香",
+            description: "提供古言、现言、原创、玄幻、都市、言情、娱乐、种田、科幻、悬疑、灵异、穿越、重生、宠文等小说,最新全本免费手机小说阅读推荐,精彩尽在红袖添香。",
+            pic: "hongxiutianxiang",
+            link: "https://www.hongxiu.com/"
+        },
+        {
+            name: "七丽时尚",
+            description: "为您提供最喜爱的时尚、美容、发型、减肥等权威资讯及各种简单易学的时尚类视频教程；如果您酷爱时尚，热爱生活，一定会喜欢七丽时尚网。",
+            pic: "qilishishang",
+            link: "http://www.7y7.com/"
+        },
+        {
+            name: "贝贝",
+            description: "1亿妈妈信赖之选，每天1折起限时特卖，童装童鞋、母婴用品、居家百货、女装鞋包等天天上新，正品保证，全场包邮。",
+            pic: "beibei",
+            link: "http://www.beibei.com/"
+        },
+        {
+            name: "她生活",
+            description: "穿衣搭配网是全球最专业服饰搭配网站。具有海量和权威的穿衣搭配指南。学习穿衣搭配的技巧，教你穿出气质，搭出风采!",
+            pic: "tashenghuo",
+            link: "http://www.shelive.net/"
+        },
+        {
+            name: "美甲帮",
+            description: "美甲帮提供美甲图片2018新款式，简单好看的美甲图片、美甲教程视频、美甲最新款式，包括法式美甲、日式美甲、光疗美甲等美甲图片和美甲知识，美甲APP应用",
+            pic: "meijiabang",
+            link: "https://www.meijiabang.cn/"
+        },
+        {
+            name: "东方女性网",
+            description: "专注于为女性朋友们供时尚潮流资讯、时尚女人搭配、潮流女人搭配等.特别是为东方女人提供全球时尚潮流资讯.女人我最大做时尚潮流女人就上东方女性网.",
+            pic: "dongfangnvxingwang",
+            link: "https://www.eastlady.cn/"
+        },
+        {
+            name: "闺蜜网",
+            description: "美妆互动媒体，收录19万款美容产品，逾2600万条用户口碑心得。从全面、实用、贴心的角度，为所有爱美人士提供全天候美妆资讯，更是你美丽消费的专业顾问",
+            pic: "guimiwang",
+            link: "http://www.kimiss.com/"
+        },
+        {
+            name: "新浪女性",
+            description: "中国著名女性网络品牌之一，汇集中文地区新鲜的女性资讯，全方位解读时尚、美妆新趋势，倡导健康生活方式，分享动人情感两性故事，引领精致女性风尚，全力打造专属于“她时代”的高端女性网络平台。",
+            pic: "xinlangnvxing",
+            link: "http://eladies.sina.com.cn/"
+        },
+        {
+            name: "爱逛街",
+            description: "一款阿里主打的手机购物软件,专门为女性用户打造。这款软件中女性使用的商品应有尽有,是一款在家购物...",
+            pic: "aiguangjie",
+            link: "https://lady.taobao.com/"
+        },
+        {
+            name: "爱丽时尚",
+            description: "作为领先的专业时尚类、生活方式类网站，专注于时装配饰、时尚产业、美容美体、护肤彩妆、珠宝腕表、新潮数码、汽车生活、艺术收藏、运动享乐等品质生活领域，爱丽时尚网全面打造融专业性、实用性",
+            pic: "ailishishang",
+            link: "http://www.aili.com/"
+        },
+        {
+            name: "维度女性",
+            description: "服务于中高收入群体的时尚女性综合门户网站，专注提供女性网潮流资讯,女性潮流服饰,女性网美容,女性网美体,女性网奢侈品,女性网健康,女性网情感,居家,亲子,女性网生活等全方位资讯",
+            pic: "weidunvxing",
+            link: "http://www.vdolady.com/"
+        }
+    ],
+    lvyou: [
+        {
+            name: "马蜂窝",
+            description: "靠谱的旅游攻略,自由行,自助游分享社区,海量旅游景点图片、游记、交通、美食、购物等自由行旅游攻略信息,马蜂窝旅游网获取自由行,自助游攻略信息更全面",
+            pic: "mafengwo",
+            link: "http://www.mafengwo.cn/"
+        },
+        {
+            name: "携程",
+            description: "中国领先的在线旅行服务公司,向超过9000万会员提供酒店预订、酒店点评及特价酒店查询、机票预订、飞机票查询、时刻表、票价查询、航班查询、度假预订、商旅",
+            pic: "xiecheng",
+            link: "http://www.ctrip.com/"
+        },
+        {
+            name: "飞猪",
+            description: "阿里巴巴旗下的综合性旅游出行服务平台。飞猪整合数千家机票代理商、航空公司、旅行社、旅行代理商资源,...",
+            pic: "feizhu",
+            link: "https://www.fliggy.com/"
+        },
+        {
+            name: "去哪儿网",
+            description: "提供机票,飞机票,特价机票,打折机票的查询预订；99元春秋航空特惠折扣机票，百元南航、海航惊喜特价机票任您挑选,国航、深航1折特价机票和折扣机票一网打尽，更多打折机票尽在Qunar.com",
+            pic: "qunaerwang",
+            link: "https://www.qunar.com/"
+        },
+        {
+            name: "缤客",
+            description: "提供全世界国内外120,000多个目的地酒店在线预订且可享受超值优惠。查询符合您预算并提供最低价格保证的酒店。",
+            pic: "binke",
+            link: "https://www.booking.com/"
+        },
+        {
+            name: "途牛",
+            description: "提供跟团游、自助游、邮轮旅游、自驾游、定制游以及景点门票预订、机票预订、火车票预订服务,还有牛人专线、首付出发旅游等品质高端、价格实惠的旅游路线.全年有各种尾货特价",
+            pic: "tuniu",
+            link: "http://www.tuniu.com/"
+        },
+        {
+            name: "同程旅游",
+            description: "一家专业的一站式旅游预订平台，提供近万家景点门票、特价机票、出国旅游、周边游、自驾游及酒店预订服务；专业旅游线路服务、让您的旅行更安心！",
+            pic: "tongchenglvyou",
+            link: "https://www.ly.com/"
+        },
+        {
+            name: "艺龙",
+            description: "国领先的在线旅行服务提供商，纳斯达克上市公司(NASDAQ: LONG)。提供全球57万家酒店预订服务，2015年艺龙用户入住了4000万以上的间夜。低价有房保障，到店无房赔付",
+            pic: "yilong",
+            link: "http://www.elong.com/"
+        }
+    ]
 }
 

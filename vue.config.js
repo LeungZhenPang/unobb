a21d518e8859480af03b47be904b58d54cd6fefa
LeungZhenPang <573750655@qq.com>
2020-05-05 01:00:37 +0800

    配置图片 limit 小于20000 转 base64

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
     devServer: {
         proxy: 'http://localhost:8080',
         public: 'localhost:8080'  // 本地ip
+    },
+    chainWebpack: config => {
+    config.module
+        .rule('images')
+        .use('url-loader')
+        .loader('url-loader')
+        .tap(options => Object.assign(options, { limit: 20000 }))
     }
 }
